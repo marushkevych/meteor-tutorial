ac3d2368a4cc67f4f59abcf025e7bf079730166d
marushkevych <marushkevych@gmail.com>
2015-11-11 23:15:30 -0500

    redirecting to list on create

--- client/addList.js
+++ client/addList.js
@@ -2,7 +2,9 @@ Template.addList.events({
   'submit form': function (event) {
     event.preventDefault();
     var input = event.target.listName;
-    Lists.insert({name: input.value});
+    Lists.insert({name: input.value}, function (err, result) {
+      Router.go('listPage', {_id: result});
+    });
     input.value = '';
   }
 });
